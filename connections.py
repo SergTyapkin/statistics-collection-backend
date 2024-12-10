8e566a2a6cf537ef23ce57c897b8a619cb776f12
Сергей Тяпкин <Tyapkin2002@mail.ru>
2024-12-10 16:23:52 +0300

    fix

--- connections.py
+++ connections.py
@@ -5,6 +5,12 @@ from utils.utils import read_config
 
 
 config = read_config('config.json')
+print("Connecting to database on:")
+print("host:", config['db_host'])
+print("port:", config['db_port'])
+print("user:", config['db_user'])
+print("db:", config['db_db'])
+print("password:", "**** (hidden)")
 DB = Database(
     host=config['db_host'],
     port=config['db_port'],
